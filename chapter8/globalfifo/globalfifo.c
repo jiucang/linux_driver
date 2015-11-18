3b8da8456e593f378df80bfef793a13b66b5cecb
jacobtao03 <jacobtao03@gmail.com>
2015-11-18 08:00:11 +0800

    globalfifo block/nonblock io part2

--- chapter8/globalfifo/globalfifo.c
+++ chapter8/globalfifo/globalfifo.c
@@ -14,18 +14,18 @@
 #include <asm/io.h>
 #include <asm/uaccess.h>
 
-#include <asm/wait.h>
+//#include <asm/wait.h>
 
-#define GLOBALINFO_SIZE 0x1000
+#define GLOBALFIFO_SIZE 0x1000
 #define MEM_CLEAR 0x1
-#define GLOBALINFO_MAJOR 250
+#define GLOBALFIFO_MAJOR 250
 
-static int globalinfo_major = GLOBALINFO_MAJOR;
+static int globalfifo_major = GLOBALFIFO_MAJOR;
 
 struct globalfifo_dev {
     struct cdev cdev;
     unsigned int current_len;
-    unsigned char mem[GLOBALINFO_SIZE];
+    unsigned char mem[GLOBALFIFO_SIZE];
 	struct mutex mutex;	/*并发控制使用的信号量*/
 	wait_queue_head_t r_wait; /*阻塞读用的等待队列头*/
 	wait_queue_head_t w_wait; /*阻塞写用的等待队列头*/
@@ -33,22 +33,22 @@ struct globalfifo_dev {
 
 struct globalfifo_dev *globalfifo_devp;
 
-int globalinfo_open(struct inode *inode, struct file *filp) {
+int globalfifo_open(struct inode *inode, struct file *filp) {
     filp->private_data = globalfifo_devp;
     return 0;
 }
 
-int globalinfo_release(struct inode *inode, struct file *filp) {
+int globalfifo_release(struct inode *inode, struct file *filp) {
     return 0;
 }
 
-long globalinfo_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
+long globalfifo_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
     struct globalfifo_dev *dev = filp->private_data;
 
     switch(cmd) {
     case MEM_CLEAR:
-        memset(dev->mem, 0, GLOBALINFO_SIZE);
-        printk(KERN_INFO "globalinfo is set to zero\n");
+        memset(dev->mem, 0, GLOBALFIFO_SIZE);
+        printk(KERN_INFO "globalfifo is set to zero\n");
         break;
     default:
         return -EINVAL;
@@ -56,7 +56,7 @@ long globalinfo_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
     return 0;
 }
 
-loff_t globalinfo_llseek(struct file *filp, loff_t offset, int method) {
+loff_t globalfifo_llseek(struct file *filp, loff_t offset, int method) {
     loff_t ret = 0;
     switch (method) {
     case 0: //SEEK_BEGIN
@@ -64,7 +64,7 @@ loff_t globalinfo_llseek(struct file *filp, loff_t offset, int method) {
             ret = -EINVAL;
             break;
         }
-        if ((unsigned int)offset > GLOBALINFO_SIZE) {
+        if ((unsigned int)offset > GLOBALFIFO_SIZE) {
             ret = -EINVAL;
             break;
         }
@@ -75,7 +75,7 @@ loff_t globalinfo_llseek(struct file *filp, loff_t offset, int method) {
             ret = -EINVAL;
             break;
         }
-        if (offset + filp->f_pos > GLOBALINFO_SIZE) {
+        if (offset + filp->f_pos > GLOBALFIFO_SIZE) {
             ret = -EINVAL;
             break;
         }
@@ -86,7 +86,7 @@ loff_t globalinfo_llseek(struct file *filp, loff_t offset, int method) {
             ret = -EINVAL;
             break;
         }
-        if (offset + filp->f_pos > GLOBALINFO_SIZE) {
+        if (offset + filp->f_pos > GLOBALFIFO_SIZE) {
             ret = -EINVAL;
             break;
         }
@@ -99,7 +99,7 @@ loff_t globalinfo_llseek(struct file *filp, loff_t offset, int method) {
     return ret;
 }
 
-ssize_t globalinfo_read(struct file *filp, char __user *user, size_t size, loff_t *offset) {
+ssize_t globalfifo_read(struct file *filp, char __user *user, size_t size, loff_t *offset) {
     int ret;
     struct globalfifo_dev *devp;
 
@@ -128,35 +128,37 @@ ssize_t globalinfo_read(struct file *filp, char __user *user, size_t size, loff_
 	}
 
     if (copy_to_user(user, (void *)(devp->mem), size)) {
-        return -EFAULT;
+        ret = -EFAULT;
+        goto out;
     } else {
-        devp->current_len = devp->current_len + size;
+        memcpy(devp->mem, devp->mem+size, devp->current_len-size);
+        devp->current_len -= size;
         ret = devp->current_len;
-        printk(KERN_INFO "read %u byte(s), current_len=%lu\n", (unsigned int)size, (long unsigned int)*ret);
-    }
+        printk(KERN_INFO "read %u byte(s), current_len=%lu\n", (unsigned int)size, (long unsigned int)ret);
 
-	wake_up_interruptible(&devp->w_wait);
+	    wake_up_interruptible(&devp->w_wait);
+    }
 
 out:
 	mutex_unlock(&devp->mutex);
 out2:
-	remove_wait_queue(&devp->r_wait, rwait);
+	remove_wait_queue(&devp->r_wait, &rwait);
 	set_current_state(TASK_RUNNING);
     return ret;
 }
 
-ssize_t globalinfo_write(struct file *filp, const char __user *user, size_t size, loff_t *offset) {
+ssize_t globalfifo_write(struct file *filp, const char __user *user, size_t size, loff_t *offset) {
     int ret;
     struct globalfifo_dev *devp;
 
-	DECLARE_WAITQUEUE(&wwait, current);
+	DECLARE_WAITQUEUE(wwait, current);
 
     devp = filp->private_data;
 
 	add_wait_queue(&devp->w_wait, &wwait);
 	mutex_lock_interruptible(&devp->mutex);
 
-	while (devp->current_len >= GLOBALINFO_SIZE) {
+	while (devp->current_len >= GLOBALFIFO_SIZE) {
 		if (filp->f_flags & O_NONBLOCK) {
 			ret = -EAGAIN;
 			goto out;
@@ -174,49 +176,52 @@ ssize_t globalinfo_write(struct file *filp, const char __user *user, size_t size
 
     if (copy_from_user((char *)(devp->mem + devp->current_len), user, size)) {
         ret = -EFAULT;
+        goto out;
     } else {
-		devp->current_len = devp->current_len + size;
+		devp->current_len += size;
+        ret = devp->current_len;
         printk(KERN_INFO "written %u byte(s),  current_len=%lu\n", (unsigned int)size,  (long unsigned int)devp->current_len);
+	    wake_up_interruptible(&devp->r_wait);
     }
-	wake_up_interruptible(&devp->r_wait);
 
 out:
 	mutex_unlock(&devp->mutex);
 out2:
 	remove_wait_queue(&devp->w_wait, &wwait);
+    set_current_state(TASK_RUNNING);
     return ret;
 }
 
-static const struct file_operations globalinfo_fops = {
+static const struct file_operations globalfifo_fops = {
     .owner = THIS_MODULE,
-    .open = globalinfo_open,
-    .read = globalinfo_read,
-    .write = globalinfo_write,
-    .llseek = globalinfo_llseek,
-    .unlocked_ioctl = globalinfo_ioctl,
-    .release = globalinfo_release,
+    .open = globalfifo_open,
+    .read = globalfifo_read,
+    .write = globalfifo_write,
+    .llseek = globalfifo_llseek,
+    .unlocked_ioctl = globalfifo_ioctl,
+    .release = globalfifo_release,
 };
 
-static void globalinfo_setup_chrdev(struct globalfifo_dev *devp, int minor) {
-    int err, devno = MKDEV(globalinfo_major, minor);
+static void globalfifo_setup_chrdev(struct globalfifo_dev *devp, int minor) {
+    int err, devno = MKDEV(globalfifo_major, minor);
 
-    cdev_init(&devp->cdev, &globalinfo_fops);
+    cdev_init(&devp->cdev, &globalfifo_fops);
     devp->cdev.owner = THIS_MODULE;
     err = cdev_add(&devp->cdev, devno, 1);
     if (err)
-        printk(KERN_NOTICE "Error %d add globalinfo device, minor %d\n", err, minor);
+        printk(KERN_NOTICE "Error %d add globalfifo device, minor %d\n", err, minor);
 }
 
-int __init globalinfo_init(void) {
+int __init globalfifo_init(void) {
     int ret;
-    int devno = MKDEV(globalinfo_major, 0);
+    int devno = MKDEV(globalfifo_major, 0);
 
     //Assigned the dev major youself
-    if (globalinfo_major)
-        ret = register_chrdev_region(devno, 1, "globalinfo");
+    if (globalfifo_major)
+        ret = register_chrdev_region(devno, 1, "globalfifo");
     else {
-        ret = alloc_chrdev_region(&devno, 0, 1, "globalinfo");
-        globalinfo_major = MAJOR(devno);
+        ret = alloc_chrdev_region(&devno, 0, 1, "globalfifo");
+        globalfifo_major = MAJOR(devno);
     }
 
     if (ret < 0)
@@ -229,12 +234,12 @@ int __init globalinfo_init(void) {
     }
     memset(globalfifo_devp, 0, sizeof(struct globalfifo_dev));
 
-    globalinfo_setup_chrdev(globalfifo_devp, 0);
+    globalfifo_setup_chrdev(globalfifo_devp, 0);
 
-	globalinfo_devp->current_len = 0;
-	mutex_init(&globalinfo_devp->mutex);	/*初始化互斥体*/
-	init_wait_queue_head(&globalinfo_devp->r_wait);
-	init_wait_queue_head(&globalinfo_devp->w_wait);		/*初始化读写等待队列*/
+	globalfifo_devp->current_len = 0;
+	mutex_init(&globalfifo_devp->mutex);	/*初始化互斥体*/
+	init_waitqueue_head(&globalfifo_devp->r_wait);
+	init_waitqueue_head(&globalfifo_devp->w_wait);		/*初始化读写等待队列*/
     return 0;
 
 fail_malloc:
@@ -242,15 +247,15 @@ fail_malloc:
     return 0;
 }
 
-void __exit globalinfo_exit(void) {
+void __exit globalfifo_exit(void) {
     cdev_del(&globalfifo_devp->cdev);
     kfree(globalfifo_devp);
-    unregister_chrdev_region(MKDEV(globalinfo_major, 0), 0);
+    unregister_chrdev_region(MKDEV(globalfifo_major, 0), 0);
 }
 
-module_param(globalinfo_major, int, S_IRUGO);
-module_init(globalinfo_init);
-module_exit(globalinfo_exit)
+module_param(globalfifo_major, int, S_IRUGO);
+module_init(globalfifo_init);
+module_exit(globalfifo_exit)
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Barry Song <21 cnbao@gmail.com>");
